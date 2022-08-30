162f96fba929f5f29488e59ac74412cd3683ca09
1 <cccamboy75@gmail.com>
2022-08-31 01:18:22 +0300

    Initial commit

---
+++ dzz2.py
@@ -0,0 +1,14 @@
+n = input("Введите 4-ох значное число: ")
+n = int(n)
+d1 = n % 10
+n = n // 10
+d2 = n % 10
+n = n // 10
+d3 = n % 10
+n = n // 10
+d4 = n % 10
+n = n // 10
+print(d4)
+print(d3)
+print(d2)
+print(d1)
